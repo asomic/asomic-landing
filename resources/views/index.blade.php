3b26ddbbfb1f7139433251440903db4b583fe5c4
Juan <jberrioscifuentes@gmail.com>
2019-02-28 12:11:45 -0300

    YEAR

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -193,7 +193,7 @@
             <img src="{{ asset('/img/logo.png') }}" alt="asomic">
           </div>
           <div class="copy">
-            <p>asomic 2019. Todos los derechos reservados</p>
+            <p>asomic 2018-{{ date('Y') }}. Todos los derechos reservados</p>
           </div>
           <div class="rrss">
             <a href="#"><img src="{{ asset('/ico/ico-facebook.svg') }}" alt="Facebook"></a>
