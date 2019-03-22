f48db7909ab82027ecc689078f143964deadbaa0
Juan <jberrioscifuentes@gmail.com>
2019-03-22 10:17:24 -0300

    favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,29 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>asomic - Agencia Digital</title>
+
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('/fav/apple-touch-icon-57x57.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('/fav/apple-touch-icon-114x114.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('/fav/apple-touch-icon-72x72.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('/fav/apple-touch-icon-144x144.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{ asset('/fav/apple-touch-icon-60x60.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ asset('/fav/apple-touch-icon-120x120.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ asset('/fav/apple-touch-icon-76x76.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ asset('/fav/apple-touch-icon-152x152.png') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('/fav/favicon-196x196.png') }}" sizes="196x196" />
+    <link rel="icon" type="image/png" href="{{ asset('/fav/favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/png" href="{{ asset('/fav/favicon-32x32.png') }}" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{ asset('/fav/favicon-16x16.png') }}" sizes="16x16" />
+    <link rel="icon" type="image/png" href="{{ asset('/fav/favicon-128.png') }}" sizes="128x128" />
+    <meta name="application-name" content="asomic"/>
+    <meta name="msapplication-TileColor" content="#1E0440" />
+    <meta name="msapplication-TileImage" content="{{ asset('/fav/mstile-144x144.png') }}" />
+    <meta name="msapplication-square70x70logo" content="{{ asset('/fav/mstile-70x70.png') }}" />
+    <meta name="msapplication-square150x150logo" content="{{ asset('/fav/mstile-150x150.png') }}" />
+    <meta name="msapplication-wide310x150logo" content="{{ asset('/fav/mstile-310x150.png') }}" />
+    <meta name="msapplication-square310x310logo" content="{{ asset('/fav/mstile-310x310.png') }}" />
+
+
     <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Semi+Condensed:400,600,700,800" rel="stylesheet">
     <link href="{{ asset('/css/hamburgers.css') }}" rel="stylesheet">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
